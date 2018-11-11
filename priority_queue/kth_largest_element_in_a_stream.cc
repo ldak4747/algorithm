ec79d4ec42a445d80331fdd91e87306718b113a0
root <root@localhost.localdomain>
2018-11-11 14:58:31 +0800

    add priorityq_queue kth_largest_element_in_a_stream

---
+++ priority_queue/kth_largest_element_in_a_stream.cc
@@ -0,0 +1,40 @@
+#include "gtest/gtest.h"
+
+/*
+ *  设计一个类, 先用一个数组初始化, 然后可以加入新的数, 加入时要获取到第K大的数
+ *
+ *  最原生的堆运用, 既然是要找第K大的数, 那么创建一个最小堆, 即堆顶最小, 堆元素最多K个, 
+ *  达到K个时, 除非更大的值可以插入, 这样保证每次插入后, size为K的堆的堆顶元素就是当前第K大的元素
+ * */
+class KthLargest {
+public:
+    priority_queue<int, vector<int>, greater<int>> heap;
+    int K;
+    KthLargest(int k, vector<int> nums): K(k) {
+        for (auto i: nums) {
+            if (heap.size() < k) {
+                heap.push(i);
+            } else {
+                if (i > heap.top()) {
+                    heap.pop();
+                    heap.push(i);
+                }
+            }
+        }
+    }
+    
+    int add(int val) {
+        if (heap.size() < K) {
+            heap.push(val);
+        } else {
+            if (val > heap.top()) {
+                heap.pop();
+                heap.push(val);
+            }
+        }
+        
+        if (heap.size() == K) {
+            return heap.top();
+        }
+    }
+};
